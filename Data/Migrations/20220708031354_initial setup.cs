673a488e9f5e66696dd6371853ac540d6cfa60bd
Reneton <reneton13@hotmail.com>
2022-07-08 10:24:57 -0400

    Car Dealership

---
+++ Data/Migrations/20220708031354_initial setup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DealershipApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    car = table.Column<string>(nullable: true),
+                    desc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealershipApp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string car { get; set; }
+        public string desc { get; set; }
+
+        public Car()
+        {
+
+        }
+    }
+}
